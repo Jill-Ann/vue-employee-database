995c70e40962cb7465d00a0a9d4f55e7eb31462c
Jill-Ann <jillannkay@gmail.com>
2021-02-06 22:12:23 +0100

    Add employee data

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,28 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmployeeTable,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Richard Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ]
+    }
   }
 }
 </script>
