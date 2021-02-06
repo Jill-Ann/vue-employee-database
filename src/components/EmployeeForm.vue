7ff0dc465a83ae4a3f224ae8b01d7f888a428dd4
Jill-Ann <jillannkay@gmail.com>
2021-02-06 22:35:50 +0100

    Add form component

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,31 @@
+<script>
+  export default {
+    name: 'employee-form',
+    data() {
+      return {
+        employee: {
+          name: '',
+          email: '',
+        },
+      }
+    },
+  }
+</script>
+
+<template>
+  <div id="employee-form">
+    <form>
+      <label>Employee name</label>
+      <input v-model="employee.name" type="text" />
+      <label>Employee Email</label>
+      <input v-model="employee.email" type="text" />
+      <button>Add Employee</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+  form {
+    margin-bottom: 2rem;
+  }
+</style>
