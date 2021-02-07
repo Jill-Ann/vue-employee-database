bb23fb921350e650e22c300146405f32651186df
Jill-Ann <jillannkay@gmail.com>
2021-02-07 16:02:13 +0100

    Update employee table

--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -31,13 +31,35 @@
 
 <template>
   <div id="employee-table">
-    <table>
-        <tbody>
-            <tr v-for="employee in employees" :key="employee.id">
-                <td>{{ employee.name}}</td>
-                <td>{{ employee.email}}</td>
-            </tr>
-        </tbody>
+    <p v-if="employees.length < 1" class="empty-table">No employees</p>
+    <table v-else>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="employee in employees" :key="employee.id">
+          <td v-if="editing === employee.id">
+            <input type="text" v-model="employee.name" />
+          </td>
+          <td v-else>{{employee.name}}</td>
+          <td v-if="editing == employee.id">
+            <input type="text" v-model="employee.email" />
+          </td>
+          <td v-else>{{ employee.email}}</td>
+          <td v-if="editing === employee.id">
+            <button @click="editEmployee(employee)">Save</button>
+            <button class="muted-button" @click="editing = null">Cancel</button>
+          </td>
+          <td v-else>
+            <button @click="editMode(employee.id)">Edit</button>
+            <button @click="$emit('delete:employee', employee.id)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
